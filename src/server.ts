3e39c9269ef0aa003bbe6def04db868d6d63c38f
Carlos Castillo <castlec23@gmail.com>
2018-07-05 19:03:55 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import * as bodyParser from 'body-parser'
 import * as express from "express"
 import { test } from "./routers/test";
+import * as crs from 'cors'
 
 var app = express();
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3001)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json()) 
-
+app.use(crs())
 app.post('/test', test)
 
 
